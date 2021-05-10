601a884dc96b799d56619b7fe3e78d85ac32952d
RahulS43 <83762345+RahulS43@users.noreply.github.com>
2021-05-10 10:33:51 +0530

    Add files via upload

---
+++ PROJECT 1/Task 1.py
@@ -0,0 +1,3 @@
+radius=float(input("Enter the radius: "))
+area=3.14*radius*radius
+print("The area of the cirlcle with radius 1.1 is:",area)
---
+++ PROJECT 1/Task 2.py
@@ -0,0 +1,4 @@
+file=input("Enter the filename:")
+extension=file.split('.')
+print("The extension of the file is:",extension[-1])
+
